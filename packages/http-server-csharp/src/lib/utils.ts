6db7b8c7cb0ca41a75468cf1038d7d6d6c194b84
Mark Cowlishaw <markcowl@microsoft.com>
2025-04-01 13:27:53 -0700

    Handle string unions specially and fix union variants and enum references

--- packages/http-server-csharp/src/lib/utils.ts
+++ packages/http-server-csharp/src/lib/utils.ts
@@ -794,8 +794,8 @@ export async function ensureCleanDirectory(program: Program, targetPath: string)
   await program.host.mkdirp(targetPath);
 }
 
-export function isValidCSharpIdentifier(identifier: string, includeDot: boolean = false): boolean {
-  if (!includeDot) return identifier?.match(/^[A-Za-z_][\w]*$/) !== null;
+export function isValidCSharpIdentifier(identifier: string, isNamespace: boolean = false): boolean {
+  if (!isNamespace) return identifier?.match(/^[A-Za-z_][\w]*$/) !== null;
   return identifier?.match(/^[A-Za-z_][\w.]*$/) !== null;
 }
 
@@ -1346,12 +1346,23 @@ export class CSharpOperationHelpers {
           return { typeReference: "int", defaultValue: stringValue, nullableType: false };
         }
         if (typeof tsType.value === "string") {
-          return { typeReference: "string", defaultValue: tsType.value, nullableType: false };
+          const retVal = this.getTypeInfo(program, tsType.enum);
+          retVal.defaultValue = `${retVal.typeReference}.${ensureCSharpIdentifier(program, tsType, tsType.name, NameCasingType.Property)}`;
+          return retVal;
         }
         return { typeReference: code`object`, nullableType: false };
       case "Union":
         return this.getUnionInfo(program, tsType);
       case "UnionVariant":
+        if (
+          isStringEnumType(program, tsType.union) &&
+          tsType.type.kind === "String" &&
+          typeof tsType.name === "string"
+        ) {
+          const retVal = this.getUnionInfo(program, tsType.union);
+          retVal.defaultValue = `${retVal.typeReference}.${ensureCSharpIdentifier(program, tsType, tsType.name, NameCasingType.Property)}`;
+          return retVal;
+        }
         return this.getTypeInfo(program, tsType.type);
       default:
         return {
@@ -1362,7 +1373,7 @@ export class CSharpOperationHelpers {
   }
   getUnionInfo(program: Program, union: Union): EmittedTypeInfo {
     const propResult = getNonNullableTsType(union);
-    if (propResult === undefined) {
+    if (propResult === undefined || isStringEnumType(program, union)) {
       return {
         typeReference: code`${this.emitter.emitTypeReference(union)}`,
         nullableType: [...union.variants.values()].some((v) => isNullType(v.type)),
@@ -1400,6 +1411,18 @@ export function findNumericType(type: NumericLiteral): [string, string] {
   const stringValue = type.valueAsString;
   if (stringValue.includes(".") || stringValue.includes("e")) return ["double", stringValue];
   return ["int", stringValue];
+}
+
+export function isStringEnumType(program: Program, union: Union): boolean {
+  const baseType = coalesceUnionTypes(program, union);
+  if (!baseType.isBuiltIn || baseType.name !== "string") return false;
+  return ![...union.variants.values()].some(
+    (v) =>
+      (v.type.kind === "String" ||
+        v.type.kind === "StringTemplate" ||
+        v.type.kind === "StringTemplateSpan") &&
+      typeof v.name !== "string",
+  );
 }
 
 export function coalesceUnionTypes(program: Program, union: Union): CSharpType {
--- packages/http-server-csharp/test/generation.test.ts
+++ packages/http-server-csharp/test/generation.test.ts
@@ -600,6 +600,140 @@ it("handles enum, complex type properties, and circular references", async () =>
   );
 });
 
+it("handles extensible enums and discriminators for inheritance", async () => {
+  await compileAndValidateMultiple(
+    runner,
+    `
+      /** An extensible string union */
+      union PetType { /** Dog */ Dog: "dog", /** Cat */ Cat: "cat", string}
+      /** A fixed string union */
+      union AnimalType {/** Wolf */ Wolf: "wolf", /** Bear */ Bear: "bear"}
+
+      /** base discriminated type */
+      @discriminator("kind")
+      model Pet {
+        /** The disriminated type */
+        kind: PetType;
+
+        /** The name */
+        name: string;
+
+        /** Age in years */
+        age: safeint;
+      }
+      
+      /** A leaf instance */
+      model Dog extends Pet {
+        /** specific kind */
+        kind: PetType.Dog;
+      /** tail length */
+        tail: "long" | "short";
+      }
+
+      /** A leaf instance */
+      model Cat extends Pet {
+        /** specific kind */
+        kind: PetType.Cat;
+        /** hair length */
+        hair: "long" | "short" | "hairless";
+      }
+      
+      /** A base animal */
+      @discriminator("kind")
+      model Animal {
+        /** The animal */
+        kind: AnimalType;
+      }
+
+      /** A leaf animal */
+      model Wolf extends Animal {
+        kind: AnimalType.Wolf;
+        variety: "dire" | "timber" | "red";
+      }
+
+      /** A leaf animal */
+      model Bear extends Animal {
+        kind: AnimalType.Bear;
+        color: "brown" | "black" | "white";
+      }
+      `,
+    [
+      [
+        "Pet.cs",
+        [
+          "public partial class Pet",
+          "[JsonConverter(typeof(JsonStringEnumConverter))]",
+          `public PetType Kind { get; set; }`,
+          `public string Name { get; set; }`,
+          `public long Age { get; set; }`,
+        ],
+      ],
+      [
+        "Animal.cs",
+        [
+          "public partial class Animal",
+          "[JsonConverter(typeof(JsonStringEnumConverter))]",
+          `public AnimalType Kind { get; set; }`,
+        ],
+      ],
+      [
+        "Dog.cs",
+        [
+          "public partial class Dog : Pet {",
+          `public new PetType Kind { get; } = PetType.Dog;`,
+          `public string Tail { get; set; }`,
+        ],
+      ],
+      [
+        "Cat.cs",
+        [
+          "public partial class Cat : Pet {",
+          `public new PetType Kind { get; } = PetType.Cat;`,
+          `public string Hair { get; set; }`,
+        ],
+      ],
+      [
+        "Bear.cs",
+        [
+          "public partial class Bear : Animal {",
+          `public new AnimalType Kind { get; } = AnimalType.Bear;`,
+          `public string Color { get; set; }`,
+        ],
+      ],
+      [
+        "Wolf.cs",
+        [
+          "public partial class Wolf : Animal {",
+          `public new AnimalType Kind { get; } = AnimalType.Wolf;`,
+          `public string Variety { get; set; }`,
+        ],
+      ],
+      [
+        "PetType.cs",
+        [
+          "[JsonConverter(typeof(JsonStringEnumConverter))]",
+          "public enum PetType",
+          `[JsonStringEnumMemberName("dog")]`,
+          "Dog,",
+          `[JsonStringEnumMemberName("cat")]`,
+          "Cat",
+        ],
+      ],
+      [
+        "AnimalType.cs",
+        [
+          "[JsonConverter(typeof(JsonStringEnumConverter))]",
+          "public enum AnimalType",
+          `[JsonStringEnumMemberName("wolf")]`,
+          "Wolf,",
+          `[JsonStringEnumMemberName("bear")]`,
+          "Bear",
+        ],
+      ],
+    ],
+  );
+});
+
 it("processes sub-namespaces of a service", async () => {
   await compileAndValidateSingleModel(
     runner,
